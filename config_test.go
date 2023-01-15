61a5f26dad1e9d5d82c007a1eb249897c85e9366
Ben Colegate <BennyC@users.noreply.github.com>
2023-01-15 11:31:18 +0000

    Test for loading env vars into config struct

--- config_test.go
+++ config_test.go
@@ -3,6 +3,7 @@ package ghostmon_test
 import (
 	"github.com/justpark/ghostmon"
 	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
@@ -12,4 +13,14 @@ func TestLoadConfigDefaultValues(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, config.ConnectionAddress, "localhost:9001")
 	require.Equal(t, config.ConnectionType, "tcp")
+}
+
+func TestLoadConfigGrabsEnvsVars(t *testing.T) {
+	_ = os.Setenv("CONNECTION_ADDR", "localhost:1001")
+	_ = os.Setenv("CONNECTION_TYPE", "abc")
+	config, err := ghostmon.LoadConfig()
+
+	require.NoError(t, err)
+	require.Equal(t, "localhost:1001", config.ConnectionAddress)
+	require.Equal(t, "abc", config.ConnectionType)
 }
