ba1f871b04c14bdad78c46fc01509a8ce569cba5
Ben Colegate <BennyC@users.noreply.github.com>
2023-01-15 12:18:29 +0000

    Fleshed out unpostpone to only accept POST

--- http.go
+++ http.go
@@ -1,16 +1,47 @@
 package ghostmon
 
+import (
+	"net/http"
+	"time"
+)
+
 // Endpoints
 // - Status
 // - CutOver
 // - Abort
 
-type HTTPServer struct {
-	communication Communicator
+func NewHTTPServer(adapter *Communicator) *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/unpostpone", handleUnpostpone(adapter))
+	mux.HandleFunc("/status", handleUnpostpone(adapter))
+	mux.HandleFunc("/abort", handleUnpostpone(adapter))
+
+	return &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 }
 
-func NewHTTPServer(adapter Communicator) *HTTPServer {
-	return &HTTPServer{
-		communication: adapter,
+// handleUnpostpone creates a http.HandlerFunc that will only accept POST requests
+// and send an "unpostpone" command over the Communicator to the postponed gh-ost process
+func handleUnpostpone(adapter *Communicator) http.HandlerFunc {
+	return onlyAllowMethod(http.MethodPost, func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello, World"))
+	})
+}
+
+// onlyAllowMethod will perform checks on the http.Request Method and check if it
+// equals the allowed method. When a disallowed method is presented, a 405 Status
+// will be returned to the client
+func onlyAllowMethod(allowedMethod string, next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != allowedMethod {
+			writer.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		next(writer, request)
 	}
 }
--- http_test.go
+++ http_test.go
@@ -1,1 +1,51 @@
-package ghostmon
+package ghostmon_test
+
+import (
+	"bytes"
+	"github.com/justpark/ghostmon"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleUnpostponeOnlyAllowsPost(t *testing.T) {
+	testCases := []struct {
+		name   string
+		method string
+	}{
+		{
+			name:   "GET",
+			method: http.MethodGet,
+		},
+
+		{
+			name:   "PATCH",
+			method: http.MethodPatch,
+		},
+
+		{
+			name:   "DELETE",
+			method: http.MethodDelete,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			request := httptest.NewRequest(testCase.method, "/unpostpone", nil)
+			writer := httptest.NewRecorder()
+			serveRequest(t, writer, request)
+
+			require.Equal(t, http.StatusMethodNotAllowed, writer.Code)
+		})
+	}
+}
+
+func serveRequest(t *testing.T, recorder *httptest.ResponseRecorder, request *http.Request) {
+	t.Helper()
+
+	var b bytes.Buffer
+	communicator := ghostmon.NewCommunicator(&b)
+	server := ghostmon.NewHTTPServer(communicator)
+	server.Handler.ServeHTTP(recorder, request)
+}
