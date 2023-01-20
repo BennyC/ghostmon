a47d60d2044a1104099995f0a5e86116b82efad2
Ben Colegate <BennyC@users.noreply.github.com>
2023-01-20 13:16:59 +0000

    Moved methods to Communicator

--- pkg/communicators/communicators.go
+++ pkg/communicators/communicators.go
@@ -36,7 +36,7 @@ func (a *Communicator) Connect(fn func(net.Conn) error) error {
 // within the Communicator instance
 func (a *Communicator) Unpostpone() error {
 	return a.Connect(func(conn net.Conn) error {
-		return send(conn, []byte("unpostpone"), []byte{})
+		return a.Send(conn, []byte("unpostpone"))
 	})
 }
 
@@ -44,7 +44,7 @@ func (a *Communicator) Unpostpone() error {
 // within the Communicator instance
 func (a *Communicator) Panic() error {
 	return a.Connect(func(conn net.Conn) error {
-		return send(conn, []byte("panic"), []byte{})
+		return a.Send(conn, []byte("panic"))
 	})
 }
 
@@ -57,7 +57,7 @@ type Status struct {
 func (a *Communicator) Status() (*Status, error) {
 	body := make([]byte, 1024)
 	err := a.Connect(func(conn net.Conn) error {
-		return sendReceive(conn, []byte("status"), body)
+		return a.SendAndReceive(conn, []byte("status"), body)
 	})
 
 	if err != nil {
@@ -69,9 +69,9 @@ func (a *Communicator) Status() (*Status, error) {
 	}, nil
 }
 
-// send will write the msg to a net.Conn and handle any errors associated with
+// Send will write the msg to a net.Conn and handle any errors associated with
 // writing, reading or closing
-func send(conn net.Conn, w []byte, r []byte) error {
+func (_ *Communicator) Send(conn net.Conn, w []byte) error {
 	if _, err := conn.Write(w); err != nil {
 		return fmt.Errorf("failed to write command: %w", err)
 	}
@@ -83,9 +83,9 @@ func send(conn net.Conn, w []byte, r []byte) error {
 	return nil
 }
 
-// sendReceive will write the msg to a net.Conn and receive the response. Any errors associated with
+// SendAndReceive will write the msg to a net.Conn and receive the response. Any errors associated with
 // writing, reading or closing will be handled
-func sendReceive(conn net.Conn, w []byte, r []byte) error {
+func (_ *Communicator) SendAndReceive(conn net.Conn, w []byte, r []byte) error {
 	if _, err := conn.Write(w); err != nil {
 		return fmt.Errorf("failed to write command: %w", err)
 	}
