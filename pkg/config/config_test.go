39e09b54a1e24fb23634a3e9987a7d6ea496cc5f
Ben Colegate <BennyC@users.noreply.github.com>
2023-01-20 14:43:19 +0000

    Renamed config var

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -8,19 +8,19 @@ import (
 )
 
 func TestLoadConfigDefaultValues(t *testing.T) {
-	config, err := config.Load()
+	c, err := config.Load()
 
 	require.NoError(t, err)
-	require.Equal(t, config.ConnectionAddress, "localhost:9001")
-	require.Equal(t, config.ConnectionType, "tcp")
+	require.Equal(t, c.ConnectionAddress, "localhost:9001")
+	require.Equal(t, c.ConnectionType, "tcp")
 }
 
 func TestLoadConfigGrabsEnvsVars(t *testing.T) {
 	_ = os.Setenv("CONNECTION_ADDR", "localhost:1001")
 	_ = os.Setenv("CONNECTION_TYPE", "abc")
-	config, err := config.Load()
+	c, err := config.Load()
 
 	require.NoError(t, err)
-	require.Equal(t, "localhost:1001", config.ConnectionAddress)
-	require.Equal(t, "abc", config.ConnectionType)
+	require.Equal(t, "localhost:1001", c.ConnectionAddress)
+	require.Equal(t, "abc", c.ConnectionType)
 }
